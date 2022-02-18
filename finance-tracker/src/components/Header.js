4dc25cc761dc6b5fa6ac70834c6c19a94b1729b1
Victoria Slavkova <vx.slavkova@gmail.com>
2022-02-18 16:32:37 +0200

    tiny header fix

--- finance-tracker/src/components/Header.js
+++ finance-tracker/src/components/Header.js
@@ -38,7 +38,6 @@ export default function Header(){
             <Link className={styles.btn} to="/about">About us</Link>
             <Link className={styles.btn} to="/login">Login</Link>
             <Link className={styles.btn} to="/register">Register</Link>
-            <Balance><Italic>Balance:</Italic> BGN 0.00</Balance>
             <div className={styles.profileIcon}>
                 <Link className={styles.btn} to="/profile">
                     {accountText ? "Account" : <AccountCircleIcon fontSize="large" />}
@@ -53,45 +52,28 @@ export default function Header(){
 const StyledHeader = styled.header`
     position: fixed;
     z-index: 9999;
-    top: 0; left: 0; right: 0;
+    top: 0; left: 0; 
     background-color: #fff;
-    height: 60px;
+    padding: 20px;
+    height: 100vh;
+    width: 200px;
     display: flex;
-    flex-flow: row wrap;
+    flex-flow: column wrap;
     justify-content: flex-start;
-    align-items: center;
+    align-items: flex-start;
+    
     box-shadow: 0px 7px 25px rgba(0,0,0,.1);
     border-bottom: 1px solid rgba(0,0,0,.1);
 
     @media (max-width: 768px) {
         overflow: hidden;
-        height: ${props => props.status ? "auto" : "60px"};
+        transform: ${props => props.status ? `translate(0)` : `translate(-100%)`};
         flex-flow: column;
         align-items: flex-start;
 
     }
 `;
 
-const Balance = styled.div`
-    font-family: 'Roboto', sans-serif;
-    color: #606060;
-    margin-left: auto;
-    font-weight: 600;
-    margin-right: 20px;
-    transition : color .3s ease;
-    &:hover {
-        color: #176CC0;
-    }
-
-    @media (max-width: 768px) {    
-        position: absolute;
-        right: 0; top: 20px;
-    }
-`;
-
-const Italic = styled.span`
-    font-style: italic;
-`;
 const Logo = styled.img`
     margin-left: 20px;
     width: 40px;
